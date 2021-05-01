2f079250e0022e448eb7c08e03a2fbbb0403720c
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-01 16:59:28 +0530

    Implement shop page

---
+++ src/pages/shop-page/shop.component.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import SHOP_DATA from'./shop.data';
+
+class ShopPage extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        return (
+            <div>Shop page</div>
+        );
+    }
+}
+
+export default ShopPage;
