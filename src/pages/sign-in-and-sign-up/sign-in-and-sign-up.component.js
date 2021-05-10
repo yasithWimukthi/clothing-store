a05f48a77866ea999cc17b49745632ea738e76dd
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-10 18:04:29 +0530

    Implement sign in page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () =>{
+    return (
+        <div className="sign-in-and-sign-up">Sign In</div>
+    );
+}
+
+export default SignInAndSignUpPage;
