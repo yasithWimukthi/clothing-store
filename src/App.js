61d9db9fb7ddc3d0eb1656bf43deab2dd2b5bef6
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-14 22:56:21 +0530

    save user details in users collection

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop-page/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends Component {
 
@@ -19,9 +19,10 @@ class App extends Component {
 
     unsubscribeFromAuth = null;
     componentDidMount() {
-        this.unsubscribe = auth.onAuthStateChanged(user =>{
-            this.setState({currentUser:user})
-            console.log(user);
+        this.unsubscribe = auth.onAuthStateChanged(async user =>{
+            // this.setState({currentUser:user})
+            // console.log(user);
+            await createUserProfileDocument(user);
         });
     }
 
