45b83e69cef5b045feb610d7b189bd41a1f1f0ae
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-19 21:09:05 +0530

    Create action types constants

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React,{Component} from "react";
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route,Redirect} from 'react-router-dom'
 import {connect} from 'react-redux';
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -9,6 +9,7 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 import {setCurrentUser} from "./redux/user/user.actions";
+//import Redirect from "react-router-dom/es/Redirect";
 
 class App extends Component {
 
@@ -58,7 +59,11 @@ class App extends Component {
                 <Switch>
                     <Route exact path="/" component={HomePage}/>
                     <Route path="/shop" component={ShopPage} />
-                    <Route path="/signin" component={SignInAndSignUpPage} />
+                    <Route
+                        exact
+                        path="/signin"
+                        component={SignInAndSignUpPage}
+                    />
                 </Switch>
             </div>
         );
@@ -66,8 +71,12 @@ class App extends Component {
 
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser : user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser : user => dispatch(setCurrentUser((user)))
 })
 
-export default connect(null,mapDispatchToProps) (App);
+export default connect(mapStateToProps,mapDispatchToProps) (App);
