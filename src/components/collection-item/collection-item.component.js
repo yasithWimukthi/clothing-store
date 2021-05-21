1a9a419284cf11330c94dcbb3646224111515899
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-21 23:08:54 +0530

    Edit collection item component

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({id,name,price,imageUrl})=>{
     return (
@@ -12,7 +13,7 @@ const CollectionItem = ({id,name,price,imageUrl})=>{
                     <span className="name">{name}</span>
                     <span className="price">{price}</span>
                 </div>
-
+                <CustomButton inverted>Add to cart</CustomButton>
             </div>
         </div>
     );
