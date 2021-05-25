ec7b6bfa1906db4bb73edcfe259eaaf5c116805f
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-25 10:12:41 +0530

    Implement checkout page

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -1,10 +1,39 @@
 import React from "react";
-import './checkout.styles.scss';
+import {connect} from 'react-redux';
 
-const CheckoutPage = () =>{
+import './checkout.styles.scss';
+import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors";
+
+const CheckoutPage = ({cartItems,total}) =>{
     return (
-        <div>checkout</div>
+        <div className="checkout-page">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
+
+
+
+        </div>
     )
 }
 
-export default CheckoutPage;
+const mapStateToProps = state => ({
+    cartItems:selectCartItems(state),
+    total: selectCartTotal(state)
+})
+
+export default connect(mapStateToProps)(CheckoutPage);
