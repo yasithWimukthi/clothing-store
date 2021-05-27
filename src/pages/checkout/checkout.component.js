e1295634c9139c88e76460090ea3ba2743a7de06
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-27 17:23:45 +0530

    change quantity

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -26,6 +26,9 @@ const CheckoutPage = ({cartItems,total}) =>{
                 </div>
             </div>
             {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/> )}
+            <div className="total">
+                <span>TOTAL : ${total}</span>
+            </div>
         </div>
     )
 }
