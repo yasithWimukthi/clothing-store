f5cf65b9409c32e1ce03f4e10393ac0b57560b54
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-28 16:37:59 +0530

    shop component reformatted

--- src/pages/shop-page/shop.component.js
+++ src/pages/shop-page/shop.component.js
@@ -1,28 +1,25 @@
 import React,{Component} from 'react';
-import SHOP_DATA from'./shop.data';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
 import CollectionPreview from "../../components/preview-collection/preview-collection.component";
+import {selectCollections} from "../../redux/shop/shop.selectors";
 
-class ShopPage extends Component{
-    constructor(props) {
-        super(props);
+const ShopPage = ({collections}) => {
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id,...otherCollectionProps}) =>{
-                        return <CollectionPreview key={id} {...otherCollectionProps} />
+    return (
+        <div className="shop-page">
+            {
+                collections.map(({id, ...otherCollectionProps}) => {
+                    return <CollectionPreview key={id} {...otherCollectionProps} />
                 })
-                }
-            </div>
-        );
-    }
+            }
+        </div>
+    );
 }
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections : selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage);
