9fe5746ba207d103b0b12eabb967864e0c87f5ff
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-29 22:28:50 +0530

    Implement category page

--- src/pages/shop-page/shop.component.js
+++ src/pages/shop-page/shop.component.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
-import CategoryPage from "../category/category.component";
+import CollectionPage from "../collection/collection.component";
 
 const ShopPage = ({match}) => {
 
     return (
         <div className="shop-page">
             <Route exact path={`${match.path}`} component={CollectionOverview}/>
-            <Route path={`${match.path}/:categoryId`} component={CategoryPage}/>
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
             {/*<CollectionOverview />*/}
         </div>
     );
