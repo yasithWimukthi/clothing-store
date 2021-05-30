611bc425436b033adf11e934644ff904ce1e91fc
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-30 21:52:50 +0530

    Edit shop selector

--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -4,7 +4,7 @@ import {createStructuredSelector} from 'reselect';
 
 import './collections-overview.styles.scss';
 import CollectionPreview from "../preview-collection/preview-collection.component";
-import {selectCollections} from "../../redux/shop/shop.selectors";
+import {selectCollectionForPreview} from "../../redux/shop/shop.selectors";
 
 const CollectionOverview = ({collections}) =>{
     return (
@@ -17,7 +17,7 @@ const CollectionOverview = ({collections}) =>{
 }
 
 const mapStateToProps = createStructuredSelector({
-    collections : selectCollections
+    collections : selectCollectionForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOverview);
