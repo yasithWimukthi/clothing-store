2f0d793381c8bbfe1c203721713ec838a044e2d2
yasithWimukthi <it19966922@my.sliit.lk>
2021-05-31 22:17:04 +0530

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import {applyMiddleware, createStore} from "redux";
 import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
