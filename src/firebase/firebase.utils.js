90f95f5d8e224f5b2cac5d13800473178c423f25
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-01 22:20:49 +0530

    add shop data to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -37,6 +37,18 @@ export const createUserProfileDocument = async (userAuth,additionalData) =>{
     return userRef;
 }
 
+export const addCollectionAndDocuments = async (collectionKey,objectToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef,obj);
+    });
+
+    return await batch.commit();
+}
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
