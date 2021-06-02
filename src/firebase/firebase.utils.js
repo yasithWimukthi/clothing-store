472462a594e06f9000ecf5b73858ed90383cde25
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-02 21:36:20 +0530

    fetch shop data to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -49,6 +49,24 @@ export const addCollectionAndDocuments = async (collectionKey,objectToAdd) => {
     return await batch.commit();
 }
 
+export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc =>{
+        const {items,title} = doc.data();
+
+        return {
+            routeName : encodeURI(title.toLowerCase()),
+            id : doc.id,
+            title,
+            items
+        }
+    });
+
+    return transformedCollection.reduce((accumulator,collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    },{})
+}
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
