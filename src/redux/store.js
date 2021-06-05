21f6281352da97971a2c25f63cc6aecae33ba4f2
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-05 20:02:50 +0530

    install redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import logger from 'redux-logger';
 import {applyMiddleware, createStore} from "redux";
 import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
+import thunk from 'redux-thunk';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
