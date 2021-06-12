a3a5b15cafa47c60a7a2bd0a6af05fa4dfeae54f
yasithWimukthi <it19966922@my.sliit.lk>
2021-06-12 16:44:20 +0530

    implement shop saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,9 +1,20 @@
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery,call,put} from 'redux-saga/effects';
 
 import ShopActionTypes from './shop.types';
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
+import {fetchCollectionsFailure, fetchCollectionsSuccess} from "./shop.actions";
 
 export function* fetchCollectionsAsync(){
-    yield console.log("saga");
+
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap,snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    }catch(err){
+        yield put(fetchCollectionsFailure(err.message));
+    }
+
 }
 
 export function* fetchCollectionsStart(){
